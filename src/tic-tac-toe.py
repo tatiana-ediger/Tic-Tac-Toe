96f14af77939a3958195032d17920ffe099c91c3
tatiana-ediger <tatiana.ediger@gmail.com>
2021-02-22 20:53:04 -0500

    updating tic-tac-toe

--- src/tic-tac-toe.py
+++ src/tic-tac-toe.py
@@ -4,6 +4,7 @@ import copy
 
 class TicTacToeState:
     """ represents the state of a tic-tac-toe game: the board and which player's turn"""
+
     def __init__(self, board: list, player_turn: str):
         self.board = board
         self.player_turn = player_turn
@@ -17,6 +18,7 @@ class TicTacToeState:
 
 class TicTacToeAction:
     """ represents an action in a tic-tac-toe game"""
+
     def __init__(self, row: int, col: int, player: str):
         self.row = row
         self.col = col
@@ -26,7 +28,7 @@ class TicTacToeAction:
         return self.row == other_action.row and self.col == other_action.col
 
     def __str__(self):
-        return f"{self.player} plays at ({self.row}, {self.col})"
+        return f"placing {self.player} at ({self.row}, {self.col})"
 
 
 def utility(game_state: TicTacToeState) -> Union[None, int]:
@@ -41,23 +43,23 @@ def utility(game_state: TicTacToeState) -> Union[None, int]:
     bottom_left = board[2][0]
     bottom_middle = board[2][1]
     bottom_right = board[2][2]
-    x_winning_states = top_left == top_middle == top_right == 'X'\
-                        or middle_left == middle_middle == middle_right == 'X'\
-                        or bottom_left == bottom_middle == bottom_right == 'X'\
-                        or top_right == middle_right == bottom_right == 'X'\
-                        or top_middle == middle_middle == bottom_middle == 'X'\
-                        or top_left == middle_left == bottom_left == 'X'\
-                        or top_left == middle_middle == bottom_right == 'X'\
-                        or top_right == middle_middle == bottom_left == 'X'
+    x_winning_states = top_left == top_middle == top_right == 'X' \
+                       or middle_left == middle_middle == middle_right == 'X' \
+                       or bottom_left == bottom_middle == bottom_right == 'X' \
+                       or top_right == middle_right == bottom_right == 'X' \
+                       or top_middle == middle_middle == bottom_middle == 'X' \
+                       or top_left == middle_left == bottom_left == 'X' \
+                       or top_left == middle_middle == bottom_right == 'X' \
+                       or top_right == middle_middle == bottom_left == 'X'
 
-    o_winning_states = top_left == top_middle == top_right == 'O'\
-                        or middle_left == middle_middle == middle_right == 'O'\
-                        or bottom_left == bottom_middle == bottom_right == 'O'\
-                        or top_right == middle_right == bottom_right == 'O'\
-                        or top_middle == middle_middle == bottom_middle == 'O'\
-                        or top_left == middle_left == bottom_left == 'O'\
-                        or top_left == middle_middle == bottom_right == 'O'\
-                        or top_right == middle_middle == bottom_left == 'O'
+    o_winning_states = top_left == top_middle == top_right == 'O' \
+                       or middle_left == middle_middle == middle_right == 'O' \
+                       or bottom_left == bottom_middle == bottom_right == 'O' \
+                       or top_right == middle_right == bottom_right == 'O' \
+                       or top_middle == middle_middle == bottom_middle == 'O' \
+                       or top_left == middle_left == bottom_left == 'O' \
+                       or top_left == middle_middle == bottom_right == 'O' \
+                       or top_right == middle_middle == bottom_left == 'O'
 
     full_board = True
     for row in board:
@@ -103,14 +105,19 @@ def play(state: TicTacToeState, action: TicTacToeAction) -> TicTacToeState:
 
 
 def minimax_decision(state: TicTacToeState):
-    max_so_far = ['', float("-inf")]
-    # return max(map(lambda action: min_value(play(state, action), possible_actions(state))))
+    action_utility_pairs = []
     for action in possible_actions(state):
-        utility = min_value(play(state, action))
-        if utility > max_so_far[1]:
-            max_so_far = [action, utility]
+        if state.player_turn == 'X':
+            action_utility_pairs.append((action, min_value(play(state, action))))
+        else:
+            action_utility_pairs.append((action, max_value(play(state, action))))
 
-    return max_so_far
+    if state.player_turn == 'X':
+        result = max(action_utility_pairs, key=lambda x: x[1])
+    else:
+        result = min(action_utility_pairs, key=lambda x: x[1])
+
+    return result
 
 
 def max_value(state: TicTacToeState) -> int:
@@ -170,7 +177,6 @@ s6_board = [['O', 'O', 'X'],
             [-1, -1, 'X']]
 s6 = TicTacToeState(s6_board, 'X')
 
-
 # ---- CONSTANTS
 # A game that has not yet started
 empty_game_board = [[-1, -1, -1],
@@ -180,8 +186,8 @@ empty_game_state = TicTacToeState(empty_game_board, 'X')
 
 # A game that is unfinished
 unfinished_game_board = [['X', 'O', 'X'],
-                        [-1, 'X', -1],
-                        [-1, -1, 'O']]
+                         [-1, 'X', -1],
+                         [-1, -1, 'O']]
 unfinished_game_state = TicTacToeState(unfinished_game_board, 'O')
 
 # A game where X won
@@ -211,28 +217,68 @@ assert utility(draw_game_state) == 0
 
 # ---- TESTING POSSIBLE ACTIONS
 assert possible_actions(empty_game_state) \
-       == [TicTacToeAction(0,0,'X'), TicTacToeAction(0,1,'X'), TicTacToeAction(0,2,'X'),
-           TicTacToeAction(1,0,'X'), TicTacToeAction(1,1,'X'), TicTacToeAction(1,2,'X'),
-           TicTacToeAction(2,0,'X'), TicTacToeAction(2,1,'X'), TicTacToeAction(2,2,'X')]
+       == [TicTacToeAction(0, 0, 'X'), TicTacToeAction(0, 1, 'X'), TicTacToeAction(0, 2, 'X'),
+           TicTacToeAction(1, 0, 'X'), TicTacToeAction(1, 1, 'X'), TicTacToeAction(1, 2, 'X'),
+           TicTacToeAction(2, 0, 'X'), TicTacToeAction(2, 1, 'X'), TicTacToeAction(2, 2, 'X')]
 
-assert possible_actions(unfinished_game_state) == [TicTacToeAction(1,0,'O'), TicTacToeAction(1,2,'O'),
-                                                  TicTacToeAction(2,0,'O'), TicTacToeAction(2,1,'O')]
+assert possible_actions(unfinished_game_state) == [TicTacToeAction(1, 0, 'O'), TicTacToeAction(1, 2, 'O'),
+                                                   TicTacToeAction(2, 0, 'O'), TicTacToeAction(2, 1, 'O')]
 
 assert possible_actions(draw_game_state) == []
 
 # ---- TESTING PLAY
-assert play(empty_game_state, TicTacToeAction(0,0,'X')) == TicTacToeState([[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]], 'O')
+assert play(empty_game_state, TicTacToeAction(0, 0, 'X')) == TicTacToeState([[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
+                                                                            'O')
 
-assert play(unfinished_game_state, TicTacToeAction(2,0,'O')) \
+assert play(unfinished_game_state, TicTacToeAction(2, 0, 'O')) \
        == TicTacToeState([['X', 'O', 'X'], [-1, 'X', -1], ['O', -1, 'O']], 'X')
 
-
 # ----- ANSWERS TO QUESTIONS:
+print("Answer to #3:")
 # 3. run this file and you can see the results to this question printed out in console:
 minimax_at_s6 = minimax_decision(s6)
 print(f"minimax result from s6: \n action: {minimax_at_s6[0]}, utility: {minimax_at_s6[1]}")
+# minimax result from s6:
+#  action: placing X at (2, 0), utility: 1
 
 minimax_at_empty = minimax_decision(empty_game_state)
 print(f"minimax result from blank initial state: \n action: {minimax_at_empty[0]}, utility: {minimax_at_empty[1]}")
+# minimax result from blank initial state:
+#  action: placing X at (0, 0), utility: 0
 
-# 4.
+print("----------------------------------------")
+print("\nAnswer to #4:")
+# 4. run this file and you cansee the results to this question printed out in console!
+def compare_states(state1: TicTacToeState, state2: TicTacToeState) -> bool:
+    if state2.player_turn == 'X':
+        return minimax_decision(state1)[1] == minimax_decision(state2)[1]
+    else:
+        return minimax_decision(state2)[1] >= minimax_decision(state1)[1]
+
+
+print('placing X at (2, 2) was an optimal move: ', compare_states(empty_game_state, s1))
+#True
+
+print('placing O at (0, 0) was an optimal move: ', compare_states(s1, s2))
+#False
+#optimal decision woudl be (1,1)
+optimal_decision_at_s2 = minimax_decision(s1)
+print('\t -an optimal action at s2 would be: ', optimal_decision_at_s2[0], ' with utility', optimal_decision_at_s2[1])
+
+print('placing X at (1, 0) was an optimal move: ', compare_states(s2, s3))
+#False
+#optimal decisions would be (2,0), (0,2)
+optimal_decision_at_s3 = minimax_decision(s2)
+print('\t -an optimal action at s3 would be: ', optimal_decision_at_s3[0], ' with utility', optimal_decision_at_s3[1])
+
+print('placing O at (0, 1) was an optimal move: ', compare_states(s3, s4))
+#False
+#optimal decisions would be (0,2), (1,1), (1,2)
+optimal_decision_at_s4 = minimax_decision(s3)
+print('\t -an optimal action at s4 would be: ', optimal_decision_at_s4[0], ' with utility', optimal_decision_at_s4[1])
+
+print('placing X at (0, 2) was an optimal move: ', compare_states(s4, s5))
+#True
+
+print('placing O at (1, 2) was an optimal move: ', compare_states(s5, s6))
+#True
